f1b7c2c1b99a12347c9e2b0a7bbb6f1fbd5dd411
Himanshu Rathod <codewithhr@gmail.com>
2026-03-11 11:16:49 +0530

    Fix CORS config

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,14 +8,14 @@ app = FastAPI(title="Url-shortener")
 
 models.Base.metadata.create_all(bind=engine)
 
-# origins = [
-#     "http://localhost:3000",               # local dev
-#     "https://snapurl-app.vercel.app",    # deployed frontend
-# ]
+origins = [
+    "http://localhost:3000",               # local dev
+    "https://snapurl-app.vercel.app",    # deployed frontend
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
